14d057549e0f53526efa669c16e612df2156bb2f
Abhay <abhay@sourcewiz.co>
2022-08-02 12:51:07 +0530

    feat : cart item empty ui

--- src/components/ecommerse/cart/index.js
+++ src/components/ecommerse/cart/index.js
@@ -1,39 +1,65 @@
+import { useDispatch, useSelector } from 'react-redux'
 
+import styled from 'styled-components'
 
-
-import { useDispatch , useSelector } from "react-redux";
-
+import EmptyCart from '../../../images/emptycart.svg'
+import './cart.css'
 
 const Cart = () => {
+  const selector = useSelector((state) => state.cartReducer)
+
+  console.log(selector.cartItems)
+
+  const NotFoundItem = () => {
+    return (
+      <div style={{ background: '#000000' }}>
+        <CartContainer>
+          <div className="cart__contain">
+            <img className="cart__img" src={EmptyCart} alt="img" />
+
+            <h1 className="empty">Empty Cart</h1>
+
+            <p style={{ textAlign: 'center', color: '#fff' }}>
+              You have no items added to cart! Start shopping.
+            </p>
+
+            <button className="shop__nowbtn">Shop Now</button>
+          </div>
+        </CartContainer>
+      </div>
+    )
+  }
+
+  return <>
+  {
+
+    selector?.cartItems.length ?  <>
 
 
-      const selector = useSelector(state => state.cartReducer);
+    <div className='cart__display'>
 
-      console.log(selector.cartItems);
-
-
-      const NotFoundItem = () => {
-
-
-            return (
-                <div>
+    </div>
 
 
 
-                </div>
-            )
-      }
 
 
+    
+    
+    
+    </> : NotFoundItem()
 
-      return (
+  }
 
-          <>
-
-
-          
-          </>
-      )
+      
+  
+  </>
 }
 
-export default Cart;
+export default Cart
+
+const CartContainer = styled.div`
+  display: flex;
+  padding: 2rem 0;
+  width: 90%;
+`
